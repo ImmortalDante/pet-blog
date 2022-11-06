fd340369984cd68987e50ba820486b891c531ffa
ImmortalDante <maklakovfdr@gmail.com>
2022-11-06 14:31:57 +0300

    v0.1.0

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Post
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug")
+    search_fields = ("title", )
+    prepopulated_fields = {"slug": ("title", )}
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "time_created", "time_updated")
+    search_fields = ("title", )
+    repopulated_fields = {"slug": ("title",)}
+    list_filter = ("time_created", )
+    readonly_fields = ("time_created", "time_updated")
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Category(models.Model):
+    slug = models.CharField(max_length=255, unique=True, db_index=True)
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("", kwargs={"slug": self.slug})
+
+    class Meta:
+        verbose_name = "Categories"
+        verbose_name_plural = "Categories"
+        ordering = ["id"]
+
+
+class Post(models.Model):
+    slug = models.CharField(max_length=255, unique=True, db_index=True)
+    title = models.CharField(max_length=120)
+    subtitle = models.CharField(max_length=255)
+    body = models.TextField()
+    image = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    time_created = models.DateTimeField(auto_now_add=True)
+    time_updated = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def _get_category_slug(self):
+        category = Category.objects.get(self.category)
+        return category.slug
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("", kwargs={
+            "category_slug": self._get_category_slug,
+            "post_slug": self.slug,
+        })
+
+    class Meta:
+        verbose_name = "Posts"
+        verbose_name_plural = "Posts"
+        ordering = ["id", "time_create"]
