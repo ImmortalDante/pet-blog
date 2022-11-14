19fee819db7fa798f76606621441182b3f14b8eb
ImmortalDante <maklakovfdr@gmail.com>
2022-11-14 13:22:35 +0300

    v0.2.1 debug toolbar

--- petBlog/settings.py
+++ petBlog/settings.py
@@ -34,10 +34,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "blog.apps.BlogConfig",
+    "debug_toolbar",
     # "account.apps.AccountConfig",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,3 +130,8 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # LOGIN_REDIRECT_URL = "/"
+
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- petBlog/urls.py
+++ petBlog/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from petBlog import settings
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("blog.urls"))
+    path("admin/", admin.site.urls),
+    path("", include("blog.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns = [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ] + urlpatterns
